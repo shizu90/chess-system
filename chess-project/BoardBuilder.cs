1b6a1c0f327878dcc5e575e9d1e29b32a3285ff7
Gabriel <soramoretto@gmail.com>
2022-07-12 15:01:49 -0300

    Melhorando interface do tabuleiro

--- chess-project/BoardBuilder.cs
+++ chess-project/BoardBuilder.cs
@@ -9,11 +9,28 @@ namespace chess_project {
     internal class BoardBuilder {
         public static void printBoard(ChessBoard board) {
             for(int i=0; i < board.rows; i++) {
+                Console.Write(8 - i + "| ");
                 for(int j=0; j < board.cols; j++) {
-                    if (board.piece(i, j) == null) Console.Write("- ");
-                    Console.Write(board.piece(i, j) + " ");
+                    if (board.piece(new Position(i, j)) == null) {
+                        Console.Write("- ");
+                    } else {
+                        printPiece(board.piece(new Position(i, j)));
+                        Console.Write(" ");
+                    }
                 }
                 Console.WriteLine();
+            }
+            Console.WriteLine("   A B C D E F G H");
+        }
+
+        public static void printPiece(Piece piece) {
+            if(piece.color == Color.White) {
+                Console.Write(piece);
+            } else {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(piece);
+                Console.ForegroundColor = aux;
             }
         }
     }
--- chess-project/Program.cs
+++ chess-project/Program.cs
@@ -1,10 +1,12 @@
 ﻿using chess_project.Board;
+using chess_project.Rules;
 
 namespace chess_project {
     class Program { 
         static void Main(string[] args) {
             ChessBoard board = new ChessBoard(8, 8);
             try {
+                board.putPiece(new King(board, Color.Black), new Position(0, 0));
                 BoardBuilder.printBoard(board);
             } catch (BoardException e) {
                 Console.WriteLine(e.Message);

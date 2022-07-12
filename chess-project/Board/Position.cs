b8240cd64907830a252104c6bfdc0ef9a78111e0
Gabriel <soramoretto@gmail.com>
2022-07-12 13:27:21 -0300

    Classe posição criada

---
+++ chess-project/Board/Position.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chess_project.Board {
+    internal class Position {
+        public int row { get; set; }
+        public int col { get; set; }
+
+        public Position(int row, int col) {
+            this.row = row;
+            this.col = col;
+        }
+    
+    }
+}
--- chess-project/Program.cs
+++ chess-project/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace chess_project {
+    class Program { 
+        static void Main(string[] args) {
+
+        }
+    }
+}

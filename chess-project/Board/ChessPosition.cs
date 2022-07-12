5daba9dc8b4268eb54c1ae4e6b76c9ca87d7fff0
Gabriel <soramoretto@gmail.com>
2022-07-12 14:22:33 -0300

    Criando classe de posição do tabuleiro

---
+++ chess-project/Board/ChessPosition.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chess_project.Board {
+    internal class ChessPosition {
+        public char column { get; set; }
+        public int row { get; set; }
+
+        public ChessPosition(char column, int row) {
+            this.column = column;
+            this.row = row;
+        }
+
+        public Position toMatrixPosition() {
+            return new Position(8 - this.row, this.column - 'a');
+        }
+
+        public override string ToString() {
+            return "" + this.column + this.row;
+        }
+    }
+}
